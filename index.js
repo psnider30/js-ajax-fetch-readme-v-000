216a8155f44cfe5a34be17fef2e75c3f97058313
psnider30 <pbsnider@gmail.com>
2017-10-30 12:23:27 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+// Learn.co peronal github token: 63c4b2373743462d95d592d579f6bad60f5573c5
 const app = "I don't do much.";
+const token = '63c4b2373743462d95d592d579f6bad60f5573c5'
+
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
